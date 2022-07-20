7380e3f4c43601f357fcf5ccd059d852fe7629a7
Neil Sambhu <neilsambhu@gmail.com>
2022-07-20 17:23:37 -0400

    apptainer download:in progress

--- run_custom_setup/quick_installation_steps.sh
+++ run_custom_setup/quick_installation_steps.sh
@@ -14,16 +14,17 @@ sudo apt install -y \
 
 # Install Go
 # https://go.dev/doc/install
+export VERSION=1.18.4
 # 1. Go download.
-curl -O https://dl.google.com/go/go1.18.4.linux-amd64.tar.gz
+curl -O https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz
 # 2. Go install.
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz
 source $HOME/.profile
 # echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 source $HOME/.profile
 go version
 # Cleanup downloaded files
-rm go1.18.4.linux-amd64.tar.gz
+rm go${VERSION}.linux-amd64.tar.gz
 
 # Download Apptainer from a release
 export VERSION=1.0.3 && # adjust this as necessary \
@@ -34,5 +35,8 @@ export VERSION=1.0.3 && # adjust this as necessary \
 ./mconfig && \
     make -C builddir && \
     sudo make -C builddir install
+apptainer help
 # Cleanup downloaded files
-rm go1.18.4.linux-amd64.tar.gz
+cd ..
+rm apptainer-${VERSION}.tar.gz*
+rm -r apptainer-${VERSION}
