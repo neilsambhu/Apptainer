03061038e302adbe527d7796084f0767dfc6999b
Neil Sambhu <neilsambhu@gmail.com>
2022-07-20 16:28:14 -0400

    initial

---
+++ run_custom_setup/quick_installation_steps.sh
@@ -0,0 +1,10 @@
+# Ensure repositories are up-to-date
+sudo apt-get update
+# Install debian packages for dependencies
+sudo apt-get install -y \
+   build-essential \
+   libseccomp-dev \
+   pkg-config \
+   squashfs-tools \
+   cryptsetup \
+   curl wget git
