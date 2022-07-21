6ce30e4b81ce0933788316cd933020ece6e67365
Neil Sambhu <neilsambhu@gmail.com>
2022-07-21 19:34:09 -0400

    build container

--- 4build_a_container/build_a_container.sh
+++ 4build_a_container/build_a_container.sh
@@ -1,6 +1,20 @@
-https://apptainer.org/docs/user/main/build_a_container.html
+# https://apptainer.org/docs/user/main/build_a_container.html
 
 cd 4build_a_container
+# Cleanup
+rm carla.sif carla_latest.sif
+sudo rm -r carla
 
 # Overview
-apptainer pull docker://carlasim/carla
+# apptainer pull docker://carlasim/carla
+
+# Downloading an existing container from Docker Hub
+# sudo apptainer build carla.sif docker://carlasim/carla
+
+# Creating writable --sandbox directories
+# sudo apptainer build --sandbox carla/ docker://carlasim/carla
+# sudo apptainer shell --writable carla/
+# apptainer shell --writable carla/ # fail: does not allow access to carla directory
+
+# Building containers from Apptainer definition files
+# sudo apptainer build carla.sif carla.def
