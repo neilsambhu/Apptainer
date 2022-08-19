809ec289e14508361a600954d9fd4c360aacbbbc
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-19 18:20:05 -0400

    Update run_UF_1.sh

--- 6carla/run_UF_1.sh
+++ 6carla/run_UF_1.sh
@@ -1,7 +1,7 @@
 cd "6carla"
 
 # build container 
-apptainer build carla-0.9.13.sif docker://carlasim/carla:0.9.13 |& tee out.txt
+apptainer build --sandbox carla-0.9.13/ docker://carlasim/carla:0.9.13 |& tee out1.txt
 
 # view container
-apptainer shell carla-0.9.13.sif
+apptainer shell carla-0.9.13/
