834be11eab5cfe09a16b2082684fdf5038274cf8
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-19 18:45:07 -0400

    Update run.sh

--- 6carla/run.sh
+++ 6carla/run.sh
@@ -1,7 +1,7 @@
 cd "6carla"
 
 # build container 
-apptainer build carla-0.9.13.sif docker://carlasim/carla:0.9.13 |& tee out.txt
+# apptainer build carla-0.9.13.sif docker://carlasim/carla:0.9.13 |& tee out.txt
 # apptainer build --sandbox carla-0.9.13/ docker://carlasim/carla:0.9.13
 
 # run container
@@ -9,13 +9,15 @@ apptainer build carla-0.9.13.sif docker://carlasim/carla:0.9.13 |& tee out.txt
 # apptainer run carla-0.9.13/
 
 # view container
-apptainer shell carla-0.9.13.sif
+# apptainer shell carla-0.9.13.sif
 # apptainer shell carla-0.9.13/
 
 # execute container CarlaUE4.sh
 # apptainer exec carla-0.9.13.sif ls
 # apptainer exec carla-0.9.13/ pwd
 # bash carla-0.9.13/home/carla/CarlaUE4.sh # successful
+# bash carla-0.9.13/home/carla/CarlaUE4.sh # unsuccessful on SAMBHU19: RHEL
+bash carla-0.9.13/home/carla/CarlaUE4.sh -RenderOffscreen
 
 # execute container CarlaUE4.sh: GPU selection
 # bash carla-0.9.13/home/carla/CarlaUE4.sh
