ad15fc0e806e2da8402984f7bb19064061308114
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-19 18:33:08 -0400

    Update run_UF_0_SIF.sh

--- 6carla/run_UF_0_SIF.sh
+++ 6carla/run_UF_0_SIF.sh
@@ -4,5 +4,5 @@ cd "6carla"
 apptainer build carla-0.9.13.sif docker://carlasim/carla:0.9.13 |& tee out0.txt
 
 # view container
-apptainer shell carla-0.9.13.sif |& tee -a out0.txt
-sudo apptainer shell carla-0.9.13.sif |& tee -a out0.txt
+apptainer shell carla-0.9.13.sif
+sudo apptainer shell carla-0.9.13.sif
