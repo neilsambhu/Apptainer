2e0495f91783dada10dab1fab78de239dbb05b43
Neil Sambhu <neilsambhu@gmail.com>
2022-08-23 18:46:58 -0400

    called CarlaUE4.sh

--- 6carla/run.sh
+++ 6carla/run.sh
@@ -13,11 +13,13 @@ cd "6carla"
 # apptainer shell carla-0.9.13/
 
 # execute container CarlaUE4.sh
-# apptainer exec carla-0.9.13.sif ls
+# apptainer exec carla-0.9.13.sif ls -lah /
 # apptainer exec carla-0.9.13/ pwd
 # bash carla-0.9.13/home/carla/CarlaUE4.sh # successful
 # bash carla-0.9.13/home/carla/CarlaUE4.sh # unsuccessful on SAMBHU19: RHEL
-bash carla-0.9.13/home/carla/CarlaUE4.sh -RenderOffscreen
+# bash carla-0.9.13/home/carla/CarlaUE4.sh -RenderOffscreen
+# As of 8/23/2022 6:42:28 PM, now know how to use / to access directory inside container
+apptainer exec carla-0.9.13.sif /home/carla/CarlaUE4.sh
 
 # execute container CarlaUE4.sh: GPU selection
 # bash carla-0.9.13/home/carla/CarlaUE4.sh
