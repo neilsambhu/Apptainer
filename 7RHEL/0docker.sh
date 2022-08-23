2f2a138207730bf0c125e877be4540fc03c9fc99
Neil Sambhu <neilsambhu@gmail.com>
2022-08-23 17:14:22 -0400

    RHEL:Docker:CARLA:flag

--- 7RHEL/0docker.sh
+++ 7RHEL/0docker.sh
@@ -2,7 +2,7 @@
 cd 7RHEL
 
 # Pull the CARLA image
-docker pull carlasim/carla:0.9.13
+sudo docker pull carlasim/carla:0.9.13
 # Run the CARLA container
 # display
 sudo docker run --privileged --gpus all --net=host -e DISPLAY=$DISPLAY carlasim/carla:0.9.12 /bin/bash ./CarlaUE4.sh
