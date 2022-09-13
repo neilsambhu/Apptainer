19aa5f8900ba5a4e8300139e83ed06fb51667a2f
Neil Sambhu <neilsambhu@gmail.com>
2022-09-13 15:17:19 -0400

    lazy

--- 8singularity-carla/run.sh
+++ 8singularity-carla/run.sh
@@ -3,4 +3,9 @@
 cd 8singularity-carla
 
 # singularity build carla-0.9.13.sif docker://carlasim/carla:0.9.13
+# SINGULARITYENV_CUDA_VISIBLE_DEVICES=0 singularity exec --nv carla-0.9.13.sif /home/carla/CarlaUE4.sh
+# APPTAINERENV_CUDA_VISIBLE_DEVICES=1 singularity exec --nv carla-0.9.13.sif /home/carla/CarlaUE4.sh
+export SINGULARITYENV_CUDA_VISIBLE_DEVICES=1
 singularity exec --nv carla-0.9.13.sif /home/carla/CarlaUE4.sh
+# singularity shell --nv carla-0.9.13.sif
+# singularity exec --nv carla-0.9.13.sif CUDA_VISIBLE_DEVICES=1 /home/carla/CarlaUE4.sh
